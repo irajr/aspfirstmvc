ea19a81c52768fa9dd0ac81434bfadf805a7c592
irajr <79904881+irajr@users.noreply.github.com>
2022-01-31 23:54:21 +0530

    Add files via upload

---
+++ FirstDemo/FirstDemo/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDemo.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Customer()
+        {
+            return View();
+        }
+    }
+}
